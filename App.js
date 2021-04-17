2330b8f0f8839c5c421463b0c3fab6691d675533
Codernirmal <61540469+codernirmalnp@users.noreply.github.com>
2021-04-17 13:55:26 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import {Switch,Route} from 'react-router-dom'
+import Home from './components/views/home'
+import Signin from './components/views/signin'
+import Signup from './components/views/signup'
+import Cart from './components/Cart'
+import {Layout} from 'antd'
+import {CategoryProvider} from './context/CategoryContext'
+import ListItem from './components/Available'
+import Single from './components/Single'
+const {Content} =Layout
+const App=()=>{
+  
+  
+  return (
+    <CategoryProvider>  
+   <Navbar/>
+   <Layout>
+     <Content style={{marginTop:'4rem',padding:'2rem',height:'100vh'}}>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/cart/:id?' component={Cart}/>
+        <Route path='/signin'  component={Signin}/>
+        <Route path='/signup'  component={Signup}/>
+        <Route path='/available' component={ListItem}/>
+        <Route path='/product/:id'  component={Single}/>
+      </Switch>
+     </Content>
+  
+   </Layout>
+   </CategoryProvider>
+  )
+}
+export default App;
