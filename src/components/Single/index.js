91a6e9609d1caf0509b4c7f74ba8867751325c9f
Codernirmal <61540469+codernirmalnp@users.noreply.github.com>
2021-04-17 13:58:54 +0545

    Add files via upload

--- src/components/Single/index.js
+++ src/components/Single/index.js
@@ -1,1 +1,48 @@
+ import {useState} from 'react'
+ import {useHistory} from 'react-router-dom'
+ import {food} from './../data'
+ import {Col,Select,Button,Card,Typography,Row} from 'antd'
+ 
+ const {Title}=Typography
+ const {Meta}=Card
+ const Single=(props)=>{
+    const history=useHistory();
+    const [qty,setQty]=useState(1)
+     const id=Number(props.match.params.id);
+     const items=food.find(x=>x.id===id)  
+     const handelChange=(value)=>{
+        setQty(value)
+     
+    }
+    const AddToCartHandeler=(value)=>{
+           
+            history.push(`/cart/${value.id}?qty=${qty}`)
+    }
+    return(
+          <Row  span={24}>
+          <Col  span={24} key={items.id}>
+                <Card 
+                hoverable
+                style={{width:300,marginTop:'1rem'}}
+                cover={<img alt="" src={items.url} style={{height:200}}/>}
+                >
+                <Meta title={items.title} description={items.description.substring(0,100)}/>
+                <Select value={qty} onChange={handelChange} style={{marginTop:'1.5rem'}}>
+                              {[...Array(items.qty).keys()].map(
+                                (x) => (
+                                  <Select.Option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </Select.Option>
+                                )
+                              )}
+                            </Select>
+                <Title level={3}>{`Rs ${items.price}/`}</Title>
+                <Button type="primary" onClick={()=>AddToCartHandeler(items)} block>AddTo Cart</Button>
+                </Card>
+                </Col>
+                </Row>
+    
+    )
 
+}
+export default Single;
